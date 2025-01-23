32b683c441ebab0457e4985fd14187358f1f29da
Viktoriyadegt <94902517+Viktoriyadegt@users.noreply.github.com>
2025-01-23 15:48:01 +0300

    Update todolist-api.ts

--- src/state/api/todolist-api.ts
+++ src/state/api/todolist-api.ts
@@ -4,8 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        // Не забываем заменить API-KEY на собственный
-        'API-KEY': '26a633dd-4006-4ff0-9074-a17cafb60e67',
+       
     },
 })
 
